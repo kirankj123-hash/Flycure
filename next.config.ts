726f4064c6b96e63ec1703818a9dbf900a140b27
kirankumar kj <kirankj123@gmail.com>
2026-04-30 12:49:53 +0530

    Fix Vercel output tracing root

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  outputFileTracingRoot: path.join(__dirname, '../..'),
   experimental: {
     optimizePackageImports: ["lucide-react"],
   },
